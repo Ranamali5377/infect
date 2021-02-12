54350d260bf083dcf4e8debba53cfaff1f605e19
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-02-12 10:09:58 +0500

    Create infect.py

---
+++ infect.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+os.system("clear") 
+os.system("rm rf infect") 
+os.system(" git clone https://github.com/Tech-abm/infect") 
+os.system("cd infect") 
+os.system("python2 infect.py")
+os.system("clear") 
+print("\t Sorry,this command has been delete") 
+time.sleep(1)
+print(" Use Blueforce-Abm Command, go to my github command and pick up command") 
+os.system("xdg-open https://github.com/Tech-abm/Blueforce-Abm") 
+os.system("git pull") 
+os.system(clear") 
+os.system("exit") 
+
+
