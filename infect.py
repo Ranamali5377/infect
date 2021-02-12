b3d8ded63fdbae0a571104ae386d06c45bb07f2b
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-02-12 10:21:50 +0500

    Update infect.py

--- infect.py
+++ infect.py
@@ -1,13 +1,17 @@
-import os
-import sys
+#!/usr/bin/python2
+# coding=utf-8
 
+import os  , sys , time , datetime  
+reload(sys)
 
 os.system("clear") 
 print('')
 print('')
 print('')
+print('')
 print("\t Sorry,this command has been delete") 
 print('') 
+print('')
 print('')
 print('')
 print("\t Use Blueforce-Abm Command") 
@@ -16,6 +20,7 @@ print('')
 os.system("\tgo to my github command and pick up command") 
 print('')
 print('')
+time.sleep(5)
 os.system("xdg-open https://github.com/Tech-abm/Blueforce-Abm") 
 os.system("git pull") 
 os.system("clear") 
