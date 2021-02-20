ae73441990375907a2f977eddce40233b474707d
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-02-21 01:32:04 +0500

    Create infect.py

---
+++ infect.py
@@ -0,0 +1,11 @@
+import os
+import sys
+os.system("clear") 
+os.system("git pull") 
+os.system("clear") 
+print("\tSorry,infect cmd has expired")
+time.sleep(2)
+print("\new update cmd coming soon") 
+time.sleep(2)
+print("\t Abm script is private, so do not copy our script") 
+os.system("xdg-open https://www.facebook.com/techabm") 
