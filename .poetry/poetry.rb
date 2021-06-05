a854174f15a2742f8a6f0d6f87e26aa5c9325412
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-06-05 12:18:03 -0700

    Create poetry.rb

---
+++ .poetry/poetry.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+#encoding: utf-8
+
+require "mechanize"
+system("clear")
+sleep 2
+
+def self.logo
+  puts"
+             ████████╗ ██╗  ██╗ ██████╗  ██╗ ██╗   ██╗ ███████╗
+             ╚══██╔══╝ ██║  ██║ ██╔══██╗ ██║ ██║   ██║ ██╔════╝
+                ██║    ███████║ ██████╔╝ ██║ ██║   ██║ █████╗
+                ██║    ██╔══██║ ██╔══██╗ ██║ ╚██╗ ██╔╝ ██╔══╝
+                ██║    ██║  ██║ ██║  ██║ ██║  ╚████╔╝  ███████╗
+                ╚═╝    ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚═╝   ╚═══╝   ╚══════╝
+                                  ###
+                            #  ### #### #
+                          #  ### \/#|### |/####
+                           ##\/#/ \||/##/_/##/_#
+                      #   ###  \/###|/ \/ # ###
+                       ###_\_\_\## | #/###_/_####
+                      ## #### # \ #| /  #### ##/##
+                 #  ###__#_--###`  |{,###---###-~####
+                                 \ }{
+                                  }}{
+                                  }}{
+                                  {{}
+                             , -=-~{ .-^- _
+                                  `}
+                                   {
+                  TIPS TO SURVIVE WEB DEV BOOTCAMP"
+  puts("Fuck these haters, I'd kill 'em all if I could. Ain't scared of none of y'all, so you know my aim good.")
+  sleep 2
+  puts("Trash talked by many. Hated by some. Guess how many fucks I give? It is less than one.")
+  sleep 2
+  puts("Who gives a flying floating FUCK what the haters think?")
+  sleep 2
+  puts("")
+  sleep 2
+  system("exit")
