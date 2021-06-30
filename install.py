13bae831dfe23b07cadd05ad3bc4d1fafe2d4eee
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-06-30 14:06:27 +0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,17 @@
+import os
+import re
+import random
+
+os.system("clear")
+
+print("")
+print("")
+print("")
+
+print("The Scripts Has Been Delete").center(50)
+print("Thank you so much for using my tool").center(50)
+print("New Update Tool Coming Soon").center(50)
+
+print("")
+print("")
+print("")
